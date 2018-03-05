173e8671a0947c68dacfcf627f27984685495745
samory harris <samory070@gmail.com>
2018-03-05 22:03:43 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,8 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+
+def display_board
+ print "this is a string"
+ print "this is a book"
+ print "this is a library"
+ print "this is a outsane"
+end
